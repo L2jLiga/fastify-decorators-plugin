6ed2232273ca8d9586faf7456ac638282fec27fd
Andrey Chalkin <andrey.chalkin@sidenis.com>
2020-01-09 18:51:44 +0700

    Created structure for plugin

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    groovy
+    id("org.jetbrains.intellij") version "0.4.15"
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "net.l2jliga"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.codehaus.groovy:groovy-all:2.3.11")
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.3.1"
+    pluginName = "Fastify decorators"
+}
+intellij.setPlugins("JavaScriptLanguage")
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
