1818564c4b31433949bb11cf9e680a0bfc994ced
Andrey Chalkin <andrey.chalkin@sidenis.com>
2020-01-26 10:53:14 +0700

    feat: implemented implicit usage provider

---
+++ src/main/kotlin/net/l2jliga/fastify_decorators_support/providers/ControllerUsageProvider.kt
@@ -0,0 +1,52 @@
+package net.l2jliga.fastify_decorators_support.providers
+
+import com.intellij.codeInsight.daemon.ImplicitUsageProvider
+import com.intellij.lang.ecmascript6.psi.ES6ExportDefaultAssignment
+import com.intellij.lang.javascript.psi.JSReferenceExpression
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptClass
+import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList
+import com.intellij.psi.PsiElement
+
+const val CONTROLLER_DECORATOR_NAME = "Controller"
+
+class ControllerUsageProvider : ImplicitUsageProvider {
+    override fun isImplicitWrite(element: PsiElement) = false
+
+    override fun isImplicitRead(element: PsiElement) = false
+
+    override fun isImplicitUsage(element: PsiElement): Boolean {
+        val typeScriptClass = extractClass(element)
+        if (typeScriptClass === null) return false
+
+        return hasDecoratorApplied(typeScriptClass)
+    }
+
+    private fun extractClass(element: PsiElement) = when (element) {
+        is TypeScriptClass -> element
+        is ES6ExportDefaultAssignment -> extractClassFromDefaultExport(element)
+        else -> null
+    }
+
+    private fun extractClassFromDefaultExport(element: ES6ExportDefaultAssignment) =
+        when (val reference = element.children.last()) {
+            is TypeScriptClass -> reference
+            is JSReferenceExpression -> extractClassFromReference(reference)
+            else -> null
+        }
+
+    private fun extractClassFromReference(reference: JSReferenceExpression) =
+        when (val resolvedClass = reference.resolve()) {
+            is TypeScriptClass -> resolvedClass
+            else -> null
+        }
+
+    private fun hasDecoratorApplied(element: PsiElement): Boolean {
+        val jsAttribute = element.children.first()
+        if (jsAttribute !is JSAttributeList) return false
+
+        return jsAttribute.decorators.iterator()
+            .asSequence()
+            .filter { it.decoratorName == CONTROLLER_DECORATOR_NAME }
+            .count() != 0
+    }
+}
