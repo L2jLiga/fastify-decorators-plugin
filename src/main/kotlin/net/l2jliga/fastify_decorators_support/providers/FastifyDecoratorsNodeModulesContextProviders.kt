771c0ee10a87fc63f14e434d7f4be91d65fb5b5d
Andrey Chalkin <andrey.chalkin@sidenis.com>
2020-02-16 15:13:26 +0700

    chore: remove redundant annotations

--- src/main/kotlin/net/l2jliga/fastify_decorators_support/providers/FastifyDecoratorsNodeModulesContextProviders.kt
+++ src/main/kotlin/net/l2jliga/fastify_decorators_support/providers/FastifyDecoratorsNodeModulesContextProviders.kt
@@ -3,13 +3,11 @@ package net.l2jliga.fastify_decorators_support.providers
 import com.intellij.javascript.nodejs.library.NodeModulesDirectoryManager
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.util.CachedValueProvider
-import com.sun.istack.NotNull
 import net.l2jliga.fastify_decorators_support.FASTIFY_DECORATORS_PACKAGE
 import net.l2jliga.fastify_decorators_support.FastifyDecoratorsContextProvider
 
 class FastifyDecoratorsNodeModulesContextProviders : FastifyDecoratorsContextProvider {
-    @NotNull
-    override fun isFastifyDecoratorsContext(@NotNull psiDir: PsiDirectory): CachedValueProvider.Result<Boolean> {
+    override fun isFastifyDecoratorsContext(psiDir: PsiDirectory): CachedValueProvider.Result<Boolean> {
         val manager = NodeModulesDirectoryManager.getInstance(psiDir.project)
         val dirPath = psiDir.virtualFile.path + "/"
         var result = false
