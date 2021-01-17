99c3400bca8553d6ef53e82432e28e561585f124
Andrey Chalkin <L2jLiga@gmail.com>
2021-01-17 12:50:24 +0700

    chore: update copyright
    Signed-off-by: Andrey Chalkin <L2jLiga@gmail.com>

--- src/main/kotlin/fastify_decorators/plugin/extensions/VirtualFile.kt
+++ src/main/kotlin/fastify_decorators/plugin/extensions/VirtualFile.kt
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 Andrey Chalkin <L2jLiga> Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2019-2021 Andrey Chalkin <L2jLiga> Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package fastify_decorators.plugin.extensions
 
 import com.intellij.openapi.vfs.VirtualFile
